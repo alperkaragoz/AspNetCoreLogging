7c71a17f88ae144b6e4faf57c650c8b096c28d0b
Alper Karagöz <alperkaragoz@outlook.com>
2023-04-06 21:26:41 +0300

    Add project files.

---
+++ AspNetCoreLogging.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreLogging.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
